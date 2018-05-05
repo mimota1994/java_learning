6355f7068ecb389e5b4a662d77db76ed29eb2e41
mimota1994 <mimota1994@users.noreply.github.com>
2018-05-05 22:03:21 +0800

    重写object类的方法
    finalize
    equal
    tostring

--- src/property/Item.java
+++ src/property/Item.java
@@ -2,37 +2,54 @@ package property;
 
 public class Item {
 	
-	private Item(){
-		
-	}
 	
-	static Item jingkela ;
-	
-	static Item getItem(){
-		if(null==jingkela)
-			jingkela=new Item();
-		return jingkela;
-	}
 	
 	String name;
 	int price;
+	
+	public String toString(){
+		return name+price;
+	}
+	
+	public void finalize(){
+		System.out.println(this.name+"正在被回收");
+	}
+	
+	public boolean equals(Object o){
+		if (o instanceof Item){
+			if(((Item) o).price==this.price)
+				return true;
+		}
+		return false;
+	}
+	
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		for(int i=0;i<2;i++){
+//			Item Bloodbottle=new Item();
+//			Bloodbottle.name="血瓶";
+//			Bloodbottle.price=50;
+//			
+//			
+////			System.out.println(Bloodbottle.toString());
+//		}
+//		
 		Item Bloodbottle=new Item();
 		Bloodbottle.name="血瓶";
 		Bloodbottle.price=50;
+	
+		Item Blood=new Item();
+		Blood.name="血";
+		Blood.price=50;
 		
-		Item straw_sandals=new Item();
-		straw_sandals.name="草鞋";
-		straw_sandals.price=300;
+		System.out.println(Bloodbottle.equals(Blood));
 		
-		Item sword=new Item();
-		sword.name="长剑";
-		sword.price=350;
+		
 
 	}
 
